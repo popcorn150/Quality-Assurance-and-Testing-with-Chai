27d90f59b803b98e789b14b7346945d571c9df8e
Dark <66648063+popcorn150@users.noreply.github.com>
2023-10-07 15:15:46 +0100

    run functional test using a headless browser II

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -90,20 +90,26 @@ suite("Functional Tests with Zombie.js", function () {
     test('Submit the surname "Colombo" in the HTML form', function (done) {
       browser.fill("surname", "Colombo").then(() => {
         browser.pressButton("submit", () => {
-          browser.assert.success()
-          browser.assert.text("span#name", "Cristoforo")
-          browser.assert.text("span#surname", "Colombo")
-          browser.assert.elements("span#dates", 1)
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
 
           done();
-        })
-      })
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.equal();
-
-      done();
+      browser.fill("surname", "Vespucci").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Amerigo");
+          browser.assert.text("span#surname", "Vespucci");
+          browser.assert.elements("span#dates", 1);
+          done();
+        });
+      });
     });
   });
 });
